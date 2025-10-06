705367ad1dbbcba6075c7cad5ba40bcf535eea8c
Colin Constable <colin@constable.cc>
2025-10-06 13:20:52 -0700

    Initial commit

---
+++ bin/simpleweb.dart
@@ -0,0 +1,5 @@
+import 'package:simpleweb/simpleweb.dart' as simpleweb;
+
+void main(List<String> arguments) {
+  print('Hello world: ${simpleweb.calculate()}!');
+}
---
+++ test/simpleweb_test.dart
@@ -0,0 +1,8 @@
+import 'package:simpleweb/simpleweb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
